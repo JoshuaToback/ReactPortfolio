bbc52622b21d0eac18f570f46947cc862e680d9c
JoshuaToback <joshuatoback@gmail.com>
2023-02-11 22:31:38 -0500

    Added React App and Tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import './App.css';
+import react from 'react';
+import Test from './test';
 
 function App() {
-  return null;
+  return (
+   <>
+   <Test />
+   <h1>Hello Friend!</h1>
+   </>
+  )
 }
 
 export default App;
