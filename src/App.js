973edbdcb064aa4d5131c711d4c2dfe3629e5c78
JoshuaToback <joshuatoback@gmail.com>
2023-02-14 19:42:54 -0500

    Added Footer and Back End pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import react from "react";
 import Navbar from "./navbar";
 import BackEnd from "./pages/backend";
 import FrontEnd from "./pages/frontend";
 import Home from "./pages/home";
 import FullStack from "./pages/fullstack";
 import Contact from "./pages/contact";
-import { Route, Routes } from 'react-router-dom';
-import styles from "./styles.css";
 
+
+// import reactImg from "./images/programs/react.png"
+import github from "./images/github.png"
+import linked from "./images/linkedin.png"
+import email from "./images/eMail.png"
+
+import { Route, Routes } from "react-router-dom";
+// eslint-disable-next-line
+import styles from "./styles.css";
 
 function App() {
   return (
@@ -22,6 +28,28 @@ function App() {
           <Route path="/Contact" element={<Contact />} />
         </Routes>
       </div>
+      <footer>
+        <div className="footer">
+          <h2>Developed by Joshua Toback</h2>
+          <ul className="socials">
+            <li>
+              <a href="https://github.com/JoshuaToback">
+                <img className="appIcon" src={github} alt="github"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/joshua-toback/">
+                <img className="appIcon" src={linked} alt="linkedin"></img>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:joshuatobackdev@gmail.com">
+                <img className="appIcon" src={email} alt="email"></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
--- src/pages/backend.js
+++ src/pages/backend.js
@@ -1,3 +1,67 @@
+import tb from '../images/techBlog.png'
+import jate from '../images/JATE.png'
+
 export default function backEnd() {
-    return <h1>Back End</h1>
+  return (
+    <>
+      <div className="pageTitle">
+        <h1>Back End</h1>
+      </div>
+      <div id="grid">
+        <div className="left">
+          <h2 className="programTitle">Tech Blog</h2>
+          <div className="tb">
+            <div className="techImage">
+              <img src={tb} alt="TechBlog"></img>
+            </div>
+            <div className="techInfo">
+              <p>
+                Using Express and Handlebars, this application is a fully functional blogsite
+                that also features a login system, post editing and comments. 
+              </p>
+            </div>
+
+            <div className="siteButtons">
+              <a
+                id="github"
+                href="https://github.com/JoshuaToback/TechBlog"
+              >
+                Github
+              </a>
+              <a
+                id="live"
+                href="https://polar-cliffs-01056.herokuapp.com/"
+              >
+                Live
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">
+          <h2 className="programTitle">Just Another Text Editor</h2>
+          <div className="jate">
+            <div className="techImage">
+              <img src={jate} alt="JATE"></img>
+            </div>
+            <div className="techInfo">
+              <p>
+                This is a Progressive Web Application that allows users to edit text within either
+                their browser or offline. 
+              </p>
+            </div>
+
+            <div className="siteButtons">
+              <a id="github" href="https://github.com/JoshuaToback/TextAdjuster">
+                Github
+              </a>
+              <a id="live" href="https://salty-temple-45216.herokuapp.com/">
+                Live
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
